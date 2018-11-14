6361ba7f31996877de8e452cb2ef5ac1b69d37d0
Matteo Sacchi <matteosacchi6k@gmail.com>
2018-11-14 19:12:47 +0100

    include e app

---
+++ app.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "400px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+ $(document).ready(function () {
+        w3.includeHTML();
+    });
