9e87570a08c1d9029bcfac8e732495268509c258
Matteo Sacchi <matteosacchi6k@gmail.com>
2018-11-15 12:52:43 +0100

    menu

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "400px";
+    document.getElementById("apri").style.left = "400px";
+    // document.getElementById("apri").onlick = "closeNav()";
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("apri").style.left = "0px";
 }
 
- $(document).ready(function () {
-        w3.includeHTML();
-    });
+
+
+$("#ring_contents li a").click(function(){
+  $("#ringTabDrop").text('Ring size Format: ' + $(this).text());
+});
+
+
+// raccogli in una variabili gli elementi del menu, aggiungi evento al click -> se click togli classe
+//active da uno e aggiungila a quello cliccato, accendi href collegato e spegni precedente.
