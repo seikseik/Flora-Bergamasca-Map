8933715f16f5def767e8b8ffca0bbca1e4401faf
Matteo Sacchi <matteosacchi6k@gmail.com>
2018-11-18 18:36:39 +0100

    aggiunto array geologia in file separato

--- js/app.js
+++ js/app.js
@@ -8,17 +8,15 @@ function openNav() {
 
     document.getElementById("apri").style.left = "500px";
     document.getElementById("triangolo").classList.add("attivo");
-    // setTimeout(function(){document.getElementById("ring-size-tabs").style.position ="fixed";},200);
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("ring-size-tabs").style.width = "0px";
-    document.getElementById("ring-size-tabs").style.left = "-100px";
+    document.getElementById("ring-size-tabs").style.left = "-150px";
 
     document.getElementById("apri").style.left = "0px";
     document.getElementById("triangolo").classList.remove("attivo");
-    // document.getElementById("ring-size-tabs").style.position ="relative";
 }
 
 
@@ -32,8 +30,9 @@ $(document).ready(function () {
          $("#features").load("./bergamo.html");
    });
 
+
 let lista = document.querySelector(".dropdown-menu");
-// aggiungi link all'inizio della pagina quando cambi capitolo
 lista.addEventListener("click", function(event){
  $("#features").load("./"+event.target.id+".html");
+$(side).scrollTop(0);
 });
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 
+var titolo = "nome-completo";
+var nome = "Artemisia verlotiorum Lamotte";
 
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGVvc2VpayIsImEiOiJjajQxbzYyamYwZ3BoMnFwYW14OWJ4YzFzIn0.ftiqjSyzdVDOaclBCDp4Gg';
@@ -6,16 +8,11 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGVvc2VpayIsImEiOiJjajQxbzYyamYwZ3BoMnFwY
 
 var map = new mapboxgl.Map({
     container: 'map', // container id
-    style: 'mapbox://styles/matteoseik/cjnsxmwc23cmb2slpa6lqrsse', //hosted style id
+    style: 'mapbox://styles/matteoseik/cjon3rla12n182rp6govkw37a', //hosted style id
     center: [9.674504
 , 45.695638], // starting position
     zoom: 13 // starting zoom
 });
-
-
-
-var titolo = "nome-completo";
-var nome = "Artemisia verlotiorum Lamotte";
 
 
 
@@ -26,60 +23,25 @@ map.on('load', function() {
     data: './json/decimali.geojson'
 });
 
-  map.addLayer({
-        'id': 'prova',
-        'type': 'circle',
-        'source': 'segnalazioni',
-        'layout': {
-            'visibility': 'visible'
-        },
-        'paint': {
-            'circle-radius': 3,
-            'circle-color': 'red'
-        },
-    });
 
-    // map.setFilter('prova', ['==', titolo, nome]);
+  // map.addLayer({
+  //       'id': 'prova',
+  //       'type': 'circle',
+  //       'source': 'segnalazioni',
+  //       'layout': {
+  //           'visibility': 'visible'
+  //       },
+  //       'paint': {
+  //           'circle-radius': 3,
+  //           'circle-color': 'red'
+  //       },
+  //   });
+  //
+  //   map.setFilter('prova', ['==', titolo, nome]);
+
 
   });
 
-
-
-
-var chapters = {
-  'par1': {
-      bearing: 27,
-      center: [9.663388242592475, 45.70322207904454],
-      zoom: 13,
-      pitch: 30
-  },
-  'par2': {
-      duration: 5000,
-      center: [9.63958919379263, 45.70906309335106],
-      bearing: 0,
-      zoom: 13.5,
-      pitch: 20
-  },
-  'par3': {
-      duration: 5000,
-      center: [9.619577301514795, 45.71923272349616],
-      zoom: 13.5,
-      // speed: 0.6,
-      pitch: 20
-  },
-  'par4': {
-      duration: 5000,
-      center: [9.626645125608746, 45.69971401304966],
-      zoom: 13.5
-  },
-  'par5': {
-      duration: 5000,
-      center: [9.667469200116699, 45.72584639715879],
-      zoom: 13,
-      pitch: 20,
-
-  }
-};
 
 
 //potrei provare ad aggiungere capitoli all'ogetto chapters per tutti i file,
