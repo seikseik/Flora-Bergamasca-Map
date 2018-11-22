17bf0222f633487176369943099774460f01e8fe
Matteo Sacchi <matteosacchi6k@gmail.com>
2018-11-22 11:34:44 +0100

    popup-eliminati

--- js/main.js
+++ js/main.js
@@ -25,34 +25,32 @@ map.on('load', function() {
   });
 });
 
+
+
 // On every scroll event, check which element is on screen
 var side = document.getElementById("mySidenav");
 side.onscroll = function() {
 
-  var listaPopup = document.getElementsByClassName("mapboxgl-popup");
-  console.log(listaPopup);
-
   // cambia il titolo del menu
 
-  if (isElementOnScreenTitolo("primo")) {
-    $("#ringTabDrop").text("Bergamo");
-  }else if(isElementOnScreenTitolo("secondo")){
-      $("#ringTabDrop").text("Geologia e clima");
-  }
+      if (isElementOnScreenTitolo("primo")) {
+        $("#ringTabDrop").text("Bergamo");
+      }else if(isElementOnScreenTitolo("secondo")){
+          $("#ringTabDrop").text("Geologia e clima");
+      }
 
   // On every scroll event, check se il livello acceso corrisponde al capitolo acceso
   // altrimenti rimuove l'ultimo livello creato
 
-var d = map.getStyle().layers;
-var c = d[d.length-1].id;
+          var d = map.getStyle().layers;
+          var c = d[d.length-1].id;
 
+          if(activeChapterName !== c && c.startsWith("liv")){
+            map.removeLayer(c);
+          };
 
-if(activeChapterName !== c && c.startsWith("liv")){
-  map.removeLayer(c);
-};
 
   var chapterNames = Object.keys(chapters);
-
   for (var i = 0; i < chapterNames.length; i++) {
       var chapterName = chapterNames[i];
       if (isElementOnScreen(chapterName)) {
@@ -61,6 +59,9 @@ if(activeChapterName !== c && c.startsWith("liv")){
       }
   }
 };
+
+
+
 
 
 function addPoligono(nome2, coordinate, colore2){
@@ -89,83 +90,81 @@ map.addLayer({
 
 //  INIZIO FUNZIONE ACCENDI LIVELLO 1
 function addCerchio(filtro1, filtro2, colore, nome){
-
 var capitoli = Object.keys(chapters);
 
-  map.addLayer({
-        'id': nome,
-        'type': 'circle',
-        'source': 'segnalazioni',
-        'layout': {
-            'visibility': 'visible',
-        },
-        'paint': {
-            'circle-radius': {
-                'base': 1.75,
-                'stops': [[12, 2], [22, 180]]
-            },
-            'circle-color': colore
-        },
-    });
-
-    map.setFilter( nome, ['==', filtro1, filtro2]);
-
-
-
-// POPUP
-    map.on('click', function(e) {
-      var features = map.queryRenderedFeatures(e.point, {
-        layers: [nome] // replace this with the name of the layer
+    map.addLayer({
+          'id': nome,
+          'type': 'circle',
+          'source': 'segnalazioni',
+          'layout': {
+              'visibility': 'visible',
+          },
+          'paint': {
+              'circle-radius': {
+                  'base': 1.75,
+                  'stops': [[12, 2], [22, 180]]
+              },
+              'circle-color': colore
+          },
       });
 
-      if (!features.length) {
-        return;
-      }
-
-      var feature = features[0];
-      var popup = new mapboxgl.Popup({ offset: [0, -15] })
-        .setLngLat(feature.geometry.coordinates)
-        .setHTML('<div id=\'popup\' class=\'popup\' style=\'z-index: 10;\'>' +
-                  '<ul>' +
-                  '<li> Nome: ' + feature.properties['nome-completo'] + ' </li>' +
-                  '<li> Corotipo:  ' + feature.properties['corotipi'] + ' </li>' +
-                  '<li> Forma biologica:  ' + feature.properties['forma-bio-semp'] + ' </li>' +
-                  '<li> <a target="_blank" href="https://www.actaplantarum.org/galleria_flora/galleria1.php?lista=0&mode=1&cat=24&cid=73&aid='+feature.properties['link']+'">Actaplantarum</a></li></ul></div>')
-        .setLngLat(feature.geometry.coordinates)
-        .addTo(map);
-
-        // console.log(popup);
-    });
+      map.setFilter( nome, ['==', filtro1, filtro2]);
 
 
 
-    // Change the cursor to a pointer when the mouse is over the places layer.
-       map.on('mouseenter', nome, function () {
-           map.getCanvas().style.cursor = 'pointer';
-       });
+          // POPUP
+              map.on('click', function(e) {
+                var features = map.queryRenderedFeatures(e.point, {
+                  layers: [nome] // replace this with the name of the layer
+                });
 
-       // Change it back to a pointer when it leaves.
-       map.on('mouseleave', nome, function () {
-           map.getCanvas().style.cursor = '';
-       });
+                if (!features.length) {
+                  return;
+                }
+
+                var feature = features[0];
+                var popup = new mapboxgl.Popup({ offset: [0, -15] })
+                  .setLngLat(feature.geometry.coordinates)
+                  .setHTML('<div id=\'popup\' class=\'popup\' style=\'z-index: 10;\'>' +
+                            '<ul>' +
+                            '<li> Nome: ' + feature.properties['nome-completo'] + ' </li>' +
+                            '<li> Corotipo:  ' + feature.properties['corotipi'] + ' </li>' +
+                            '<li> Forma biologica:  ' + feature.properties['forma-bio-semp'] + ' </li>' +
+                            '<li> <a target="_blank" href="https://www.actaplantarum.org/galleria_flora/galleria1.php?lista=0&mode=1&cat=24&cid=73&aid='+feature.properties['link']+'">Actaplantarum</a></li></ul></div>')
+                  .setLngLat(feature.geometry.coordinates)
+                  .addTo(map);
+
+                  // remove popup
+                  let listaP = document.getElementsByClassName("mapboxgl-popup");
+                  if(popup.isOpen() && listaP.length > 1){
+                    for (var i = listaP.length - 1; i >= 1; --i) {
+                      listaP[i].parentNode.removeChild(listaP[i]);
+                      }
+                    }
+
+              });
 
 
 
-// RIMUOVI LIVELLO PRECEDENTE
-    var a = map.getStyle().layers;
-    var b = a[a.length-2].id;
-map.removeLayer(b);
+      // Change the cursor to a pointer when the mouse is over the places layer.
+         map.on('mouseenter', nome, function () {
+             map.getCanvas().style.cursor = 'pointer';
+         });
 
-// RIMUOVI POP-UP
+         // Change it back to a pointer when it leaves.
+         map.on('mouseleave', nome, function () {
+             map.getCanvas().style.cursor = '';
+         });
 
-// var listaPopup = document.getElementsByClassName("mapboxgl-popup");
-// console.log(listaPopup);
 
-// if(listaPopup.length >1){
-//   listaPopup.pop();
-// }
+  // RIMUOVI LIVELLO PRECEDENTE
+      var a = map.getStyle().layers;
+      var b = a[a.length-2].id;
+  map.removeLayer(b);
+
 
 };   //  FINE FUNZIONE ACCENDI LIVELLO 1
+
 
 
 
@@ -177,12 +176,12 @@ map.removeLayer(b);
 function setActiveChapter(chapterName) {
   if (chapterName === activeChapterName) return;
 
-  map.flyTo(chapters[chapterName]);
+    map.flyTo(chapters[chapterName]);
 
-  document.getElementById(chapterName).setAttribute('class', 'active');
-  document.getElementById(activeChapterName).setAttribute('class', '');
+      document.getElementById(chapterName).setAttribute('class', 'active');
+      document.getElementById(activeChapterName).setAttribute('class', '');
 
-  activeChapterName = chapterName;
+      activeChapterName = chapterName;
 
   if(activeChapterName.startsWith("liv-geo")){
     addCerchio(chapters[chapterName].filtro1, chapters[chapterName].filtro2, chapters[chapterName].colore, chapters[chapterName].nome);
@@ -191,8 +190,8 @@ function setActiveChapter(chapterName) {
   if(activeChapterName.startsWith("liv-geo3")){
     addPoligono(chapters[chapterName].nome2,chapters[chapterName].coordinate, chapters[chapterName].colore2);
   };
-
 }
+
 
 
 //  CHECK SE ELEMENT E' SU SCHERMO
