f5b21b7e6fb6fae0b213d4102fb6e6b54b8796ed
Matteo Sacchi <matteosacchi6k@gmail.com>
2019-01-04 13:28:38 +0100

    minifyJson

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt){
+
+//config
+
+grunt.initConfig({
+  concat:{
+    js:{
+      src:["js/highcharts.js","js/app.js","js/risorse.js","js/main.js"],
+      dest:"build/script.js"
+
+    }
+  },
+  uglify:{
+    build:{
+      files: [{
+        src:"build/script.js",
+        dest:"build/script.js"
+      }]
+    }
+  },
+  'json-minify': {
+  build: {
+    files: './json/segnalazioni.geojson'
+  }
+}
+
+
+});
+
+//plugins
+
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-json-minify');
+
+
+
+//register task
+grunt.registerTask("concat-js",["concat:js"]);
+
+};
